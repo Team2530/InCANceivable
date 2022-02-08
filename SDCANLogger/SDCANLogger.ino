4a841dd9bc8bec9e20f130008f61742a930b71ed
NifleySnifley <sailzforjunkmailz@gmail.com>
2022-02-07 20:54:35 -0600

    Fixed logging stuff

--- SDCANLogger/SDCANLogger.ino
+++ SDCANLogger/SDCANLogger.ino
@@ -5,6 +5,7 @@
 #include <InCANceivable.h>
 #include <FRC_CAN.h>
 #include <FRC_CAN_utils.h>
+#include <stdio.h>
 
 // For Arduino MCP2515 Hat:
 const int CAN_INT_PIN = 2;
@@ -19,7 +20,7 @@ const char filename[30] = "canlog_0.csv";
 void setup() {
   // Start a serial connection for local debugging
   // make sure to remove all `Serial` stuff when not connecting to the board over USB
-  Serial.begin(115200);
+  //Serial.begin(115200);
 
   // Specific to the Seeed CAN shield - A pin is used to send a "interrupt" (very low-level message) to the board via a specific pin.
   // In the case of the Seeed CAN shield, this pin is 2
@@ -32,21 +33,20 @@ void setup() {
   InCANceivable_setup();
 
   // Logger specific - Initializes the SD card on the Seeed shield.
-  if (!SD.begin(4)) {
-      Serial.println("SD init fail!");
-      while(true);
-  } Serial.println("SD init OK.");
+  while (!SD.begin(4)) {
+    delay(1000);
+  } // Serial.println("SD init OK.");
 
   // Make sure to create a new logfile.
   for (int logn = 0; SD.exists(filename); ++logn) {
-    sprintf(filename, "canlog_%d.csv", logn);
+    snprintf((char*)filename, 30, "canlog_%d.csv", logn);
   }
 }
 
 void loop() {
   static int canRunning = 0;
   static unsigned char lastHeartbeat[8] = { 0 };
-
+  
   // Locals for storing data received from the CAN bus
   unsigned long canID;
   unsigned char CANbuf[8];
@@ -62,7 +62,7 @@ void loop() {
       // Read the message from buffers in the CAN chip
       CAN.readMsgBufID(&canID, &CANlen, CANbuf);
 
-       // Discard first CAN packet
+      // Discard first CAN packet
       if (canRunning == 0) {
         canRunning = 1;
       } else {
@@ -93,16 +93,15 @@ void loop() {
         logFile.print(",");
         logFile.print(canID); // ID
         logFile.print(",");
-        logFile.print(messageAPIclass); // API class
-        logFile.print(",");
-        logFile.print(messageAPIindex); // API idx
-        logFile.print(",");
         logFile.print(CANlen); // Data len
 
         // Data
-        for (int i = 0; i < CANlen; ++i) {
+        for (int i = 0; i < 8; ++i) {
           logFile.print(",");
-          logFile.print(CANbuf[i]); 
+          if (i <= CANlen)
+            logFile.print(CANbuf[i]); 
+          else
+            logFile.print(0); // Pad with zeros for equal columns
         }
         
         logFile.println();
